f22ee0a242fe49a3ca7e7e6db245b9a2a86e7823
Adam Przytuła <adam.przytula3@gmail.com>
2024-11-24 12:57:51 +0100

    LiveAndScoreV2

--- Assets/TutorialSprites/Scripts/ScoreManager.cs
+++ Assets/TutorialSprites/Scripts/ScoreManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+//
 public class ScoreManager : MonoBehaviour
 {
     public Text scoreTxt, hiScoreTxt;
